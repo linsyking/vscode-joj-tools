62a35c1592bc48fd8bc83e2fb9358f951273b8e7
linsy king <linsy_king@sjtu.edu.cn>
2022-10-13 20:26:31 +0800

    feat: start writing serializer

---
+++ src/Serializer.ts
@@ -0,0 +1,13 @@
+// Save and Load the course data
+
+import { JOJItem, JOJProvider } from "./JOJDataProvider";
+
+export function loadJOJTree(jojTreeJson:string, jojRoot: JOJProvider) {
+    // Load the JOJ Tree From Json and add them to jojRoot
+
+}
+
+
+export function dumpJOJTree(jojRoot: JOJProvider){
+    // TODO: Return the JSON String
+}
